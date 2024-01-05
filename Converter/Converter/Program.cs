151adf710dd35de41c6520ff8c05c6b95739bbc2
RyanConroyBC <rjcon597@learner.dvusd.org>
2024-01-05 09:29:08 -0700

    Edited Initial Code

--- Converter/Converter/Program.cs
+++ Converter/Converter/Program.cs
@@ -10,6 +10,11 @@ namespace Converter
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
